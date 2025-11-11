56bebf8085bcfc6cbd6fa4e0381a688f2c620121
Nedim Bečić <nbecic1@etf.unsa.ba>
2025-11-12 00:13:36 +0100

    Moved tt and killer_move into separate headers. Added killer moves, negascout and null move prunning

---
+++ killer_move.h
@@ -0,0 +1,27 @@
+#include "include/chess.hpp"
+
+using namespace chess;
+
+const int MAX_DEPTH = 64;
+struct KillerMoves {
+    Move killers[MAX_DEPTH][2];
+    
+    KillerMoves() {
+        for (int i = 0; i < MAX_DEPTH; i++) {
+            killers[i][0] = Move::NO_MOVE;
+            killers[i][1] = Move::NO_MOVE;
+        }
+    }
+    
+    void addKiller(int depth, Move move) {
+        if (move != killers[depth][0]) {
+            killers[depth][1] = killers[depth][0];
+            killers[depth][0] = move;
+        }
+    }
+    
+    bool isKiller(int depth, Move move) const {
+        return move == killers[depth][0] || move == killers[depth][1];
+    }
+};
+
